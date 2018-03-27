29ad2e12b373dda68d351b4fc85def62871d0b96
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-03-27 12:03:55 +0300

    global modellink components

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import Group from '@/components/Groups/Group'
+import Host from '@/components/Hosts/Host'
+import Project from '@/components/Projects/Project'
 
+Vue.component('group', Group)
+Vue.component('project', Project)
+Vue.component('host', Host)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
