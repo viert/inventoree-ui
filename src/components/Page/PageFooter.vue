c62b2c1ee9639fc3dd041d68dda6c1e53b836a7e
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-07-04 10:37:33 +0300

    no pageheader styles in footer

--- src/components/Page/PageFooter.vue
+++ src/components/Page/PageFooter.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import { mapState } from 'vuex'
 export default {
   computed: {
     ...mapState(['appInfo', 'appInfoLoaded'])
@@ -16,18 +16,15 @@ export default {
 </script>
 
 <style>
-.PageFooter, .PageHeader {
-    display: flex;
-    width: 100%;
-    height: 54px;
-    box-sizing: border-box;
-    justify-content: space-between;
-    font-size: 0.8rem;
-}
-
 .PageFooter {
-    align-items: center;
-    padding: 0 20px;
-    border-top: 1px solid #eee;
+  display: flex;
+  width: 100%;
+  height: 54px;
+  box-sizing: border-box;
+  justify-content: space-between;
+  font-size: 0.8rem;
+  align-items: center;
+  padding: 0 20px;
+  border-top: 1px solid #eee;
 }
 </style>
