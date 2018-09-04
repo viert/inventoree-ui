ad895bf9ff075d2125e4a5d25c07b5bceca7b002
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-09-04 13:19:15 +0300

    permission errors fixed

--- src/errors/ErrorHandler.js
+++ src/errors/ErrorHandler.js
@@ -3,10 +3,18 @@ import store from '@/store'
 const ErrorHandler = err => {
   if (err.response) {
     if (err.response.status === 403) {
-      if (err.response.data && err.response.data.state === 'logged out') {
-        store.commit('setAuthState', 'login')
-        store.commit('setUser', null)
-        store.commit('setExtAuth', err.response.data)
+      if (err.response.data) {
+        if (err.response.data.state === 'logged out') {
+          store.commit('setAuthState', 'login')
+          store.commit('setUser', null)
+          store.commit('setExtAuth', err.response.data)
+        } else if (err.response.data.errors) {
+          err.response.data.errors.forEach(item => {
+            store.dispatch('error', item)
+          })
+        } else {
+          console.log(err)
+        }
       }
     } else if (err.response.status === 502 || err.response.status === 504) {
       store.dispatch(
